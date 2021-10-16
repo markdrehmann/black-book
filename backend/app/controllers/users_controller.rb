0afcd2e1d4554056935b1721f31cc4d1de5305dd
markdrehmann <markdrehmann@gmail.com>
2021-10-15 22:28:19 -0700

    Seeded db, created User controller actions

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,2 +1,14 @@
 class UsersController < ApplicationController
+  def index
+    users = User.all
+    render json: users.to_json(:include => {
+    :contacts => {:only => [:id, :first_name, :last_name, :phone, :email, :address, :user_id]},
+  }, :except => [:created_at, :updated_at])
+  end
+
+  def create
+    user = User.create(username: params[:username], password: params[:password])
+    render json: user, except: [:created_at, :updated_at]
+  end
+
 end
--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+user_a = User.create(username: "Joe", password: "test")
+user_b = User.create(username: "Tim", password: "test")
+
+contact_a = Contact.create(first_name: "Joe", last_name: "Bigs", phone: "555-012-3456", email: "joe@bigs.com", address: "New York, NY", user: user_a)
+contact_b = Contact.create(first_name: "Brian", last_name: "Smalls", phone: "555-012-9876", email: "brian@smalls.com", address: "New York, NY", user: user_a)
+
+note_a = Note.create(text: "This is a sample note", contact: contact_a)
