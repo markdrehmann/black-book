30cd804ee40777220c4bd17e17e06d3bc4e09284
markdrehmann <markdrehmann@gmail.com>
2021-10-17 22:01:48 -0700

    Editing users_controller

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,10 +1,18 @@
 class UsersController < ApplicationController
   def index
     users = User.all
-    render json: users.to_json(:include => {
-    :contacts => {:only => [:id, :first_name, :last_name, :phone, :email, :address, :user_id]},
-  }, :except => [:created_at, :updated_at])
+    render json: users, :include => {
+    :contacts => {:only => [:id, :first_name, :last_name, :phone, :email, :address, :user_id]}
+  }, :except => [:created_at, :updated_at] 
   end
+
+  def show
+    user = User.find_by(id: params[:id])
+    render json: user, :include => [
+      :contacts => {:only => [:id, :first_name, :last_name, :phone, :email, :address, :user_id]},
+      :notes => {:only => [:id, :text, :contact_id]}
+    ]
+  end # THIS HAS ALL OF THE INFO I WANT, HOWEVER I WISH NOTES WERE NESTED WITHIN THEIR CONTACTS, BUT THEY'RE IN A SEPARATE ARRAY
 
   def create
     user = User.create(username: params[:username], password: params[:password])
@@ -17,3 +25,5 @@ class UsersController < ApplicationController
     render json: user
   end
 end
+
+# render json: users, include: {id:, first_name:, last_name:, phone:, email:, address:, user_id:, notes: {include: :text}}
--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,4 @@ contact_a = Contact.create(first_name: "Joe", last_name: "Bigs", phone: "555-012
 contact_b = Contact.create(first_name: "Brian", last_name: "Smalls", phone: "555-012-9876", email: "brian@smalls.com", address: "New York, NY", user: user_a)
 
 note_a = Note.create(text: "This is a sample note", contact: contact_a)
+note_b = Note.create(text: "Another sample note", contact: contact_b)
