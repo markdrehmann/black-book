b297955690940669ce8e60dfd77eca2ebe15d3ea
markdrehmann <markdrehmann@gmail.com>
2021-10-17 22:47:35 -0700

    Setting up App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header"> ((THIS JUST HAS SOME CSS))*/} 
+      {/* <header className="App-header"> ((THIS JUST HAS SOME CSS))  */}
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           This is the Black Book, sucka.
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { thunk } from 'redux-thunk';
+import thunk from 'redux-thunk';
 import rootReducer from './reducers/rootReducer';
 // import reportWebVitals from './reportWebVitals';
 
