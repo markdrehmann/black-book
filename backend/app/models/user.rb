9f81524e62e4392fc50d6fdf2671c4a0d9f3e4b7
markdrehmann <markdrehmann@gmail.com>
2021-10-20 12:53:46 -0700

    User model validates username uniqueness

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
   has_many :contacts
   has_many :notes, through: :contacts
   validates :username, presence: true
+  validates :username, uniqueness: true
   validates :password, presence: true
 end
