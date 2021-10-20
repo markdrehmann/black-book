105fd1382c2ba1abaa360dcad2787ef6fff9570d
markdrehmann <markdrehmann@gmail.com>
2021-10-19 21:18:57 -0700

    Enabled cors in backend

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import { connect } from 'react-redux';
+import { fetchUser } from './actions/userActions';
 import {
   BrowserRouter as Router,
   Route
@@ -13,23 +14,49 @@ import NavBar from './components/NavBar';
 import Title from './components/Title';
 import User from './components/users/User';
 
-function App() {
-  return (
-    <Router>
-      <div className='App'>
-        <Title />
-        <NavBar />
-        <Route exact path="/" component={Home} />
-        <Route exact path="/signup" component={NewUser} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/users/:id" component={User} />
-        <footer className="App-footer">
-          <img src={logo} className="App-logo" alt="logo" />
-        </footer>
-      </div>
-    </Router>
-    //     <img src={logo} className="App-logo" alt="logo" />
-  );
+class App extends React.Component {
+  componentDidMount() {
+    console.log(this.props);
+    this.props.fetchUser();
+  }
+
+  render() {
+    return (
+      <Router>
+        <div className='App'>
+          <Title />
+          <NavBar />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/signup" component={NewUser} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/users/:id" component={User} />
+          <footer className="App-footer">
+            <img src={logo} className="App-logo" alt="logo" />
+          </footer>
+        </div>
+      </Router>
+      //     <img src={logo} className="App-logo" alt="logo" />
+    );
+  }
 }
 
-export default connect()(App);
+const mapStateToProps = state => {
+  return {
+    users: {
+      users: state.users.users,
+      loggedIn: state.users.loggedIn,
+      currentUserId: state.users.currentUserId,
+      loading: state.users.loading
+    },
+    contacts: state.contacts,
+    notes: state.notes
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchUser: () => dispatch(fetchUser())
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
