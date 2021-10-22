b6ddcad29bbd0ab11f53be3052803a574cb1bf2f
markdrehmann <markdrehmann@gmail.com>
2021-10-22 02:27:57 -0700

    Starting to work on Contacts

--- src/containers/contactsContainer.js
+++ src/containers/contactsContainer.js
@@ -1,6 +1,14 @@
 import React from 'react';
 
 class ContactsContainer extends React.Component {
+  renderContacts = () => {
+    return(
+      <>
+        <p>this will loop through all contacts and use the Contact component to rock 'em</p>
+      </>
+    )
+  }
+
   render() {
     return(<p>Contacts Container placeholder...</p>)
   }
