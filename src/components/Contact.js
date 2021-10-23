6409b1fa1c4db0195510b8b0d235b7a47e82c7b0
markdrehmann <markdrehmann@gmail.com>
2021-10-22 18:28:48 -0700

    Added link to Contact.js to ShowContact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,17 @@
+import { NavLink } from "react-router-dom";
+
 const Contact = props => {
   // {console.log(props)}
   return (
     <tr>
-      <td>{props.contact.first_name} {props.contact.last_name}</td>
+      <td>
+        <NavLink 
+          style={{ marginRight: '10px' }} 
+          to={`/contacts/${props.contact.id}`}
+        >
+          {props.contact.first_name} {props.contact.last_name}
+        </NavLink>
+      </td>
       <td>{props.contact.phone}</td>
       <td>{props.contact.email}</td>
       <td>{props.contact.address}</td>
