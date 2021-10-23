a1f8e43725c4c5b118e6657551e1e9dffc40546c
markdrehmann <markdrehmann@gmail.com>
2021-10-22 18:37:45 -0700

    Stubbed out reducer, removed excess code

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,15 +1,8 @@
-// import { combineReducers } from 'redux';
-
-// const rootReducer = combineReducers({
-//   users: usersReducer,
-//   contacts: contactsReducer,
-//   notes: notesReducer
-// });
-
 function rootReducer(state = {
   user: {},
   loading: false
 }, action) {
+  let idx;
   switch(action.type) {
     case 'LOADING':
       return {
@@ -33,92 +26,17 @@ function rootReducer(state = {
           ...state.user, contacts: [...state.user.contacts, action.response]},
         loading: false
       }
+    case 'DELETE_CONTACT':
+      return state // placeholder -- idx gets involved...maybe
+    case 'UPDATE_CONTACT':
+      return state // placeholder -- idx gets involved...maybe
+    case 'ADD_NOTE':
+      return state // placeholder
+    case 'DELETE_NOTE':
+      return state // placeholder -- idx gets involved...maybe
     default:
       return state;
   }
 }
 
 export default rootReducer;
-
-// probably not using anything below here -- except maybe some contact and note actions?
-
-// function usersReducer(state = {users: [], loggedIn: false, currentUserId: null, loading: false}, action) {
-//   let idx;
-//   switch(action.type) {
-//     case 'LOADING_USER':
-//       return {
-//         ...state,
-//         users: [...state.users],
-//         loading: true
-//       }
-//     case 'GET_USER':
-//       return {
-//         ...state, users: [action.users]
-//         // ...state,
-//         // users: [...state.users, action.users.users],
-//         // loggedIn: true,
-//         // currentUserId: action.id,
-//         // loading: false
-//       }
-//     case 'ADD_USER':
-//       return {
-//         users: [...state, action.user],
-//         loggedIn: true,
-//         currentUserId: action.user.id,
-//         loading: false
-//       };
-//     // case 'DELETE_USER':  // THIS CASE MAY NOT BE TOTALLY NECESSARY
-//     //   idx = state.users.findIndex(user => user.id === action.id);
-//     //   return [...state.slice(0, idx), ...state.slice(idx + 1)];
-//     case 'LOG_IN':
-//       idx = state.users.findIndex(user => user.username === action.username && user.password === action.password)
-//       return {
-//         ...state,
-//         loggedIn: true,
-//         currentUserId: state.users[idx],
-//         loading: false
-//       }
-//     case 'LOG_OUT':
-//       return {
-//         ...state,
-//         loggedIn: false,
-//         currentUserId: null,
-//         loading: false
-//       }
-//     default:
-//       return state;
-//   }
-// }
-
-// function contactsReducer(state = [], action) {
-//   let idx;
-//   switch(action.type) {
-//     case 'LOADING_CONTACTS':
-//       return state;
-//     case 'ADD_CONTACT':
-//       return [...state, action.contact];
-//     case 'DELETE_CONTACT':
-//       idx = state.findIndex(contact => contact.id === action.id)
-//       return [...state.slice(0, idx), ...state.slice(idx + 1)];
-//     case 'UPDATE_CONTACT':
-//       idx = state.findIndex(contact => contact.id === action.id)
-//       return state;
-//     default:
-//       return state;
-//   };
-// };
-
-// function notesReducer(state = [], action) {
-//   let idx;
-//   switch(action.type) {
-//     case 'LOADING_NOTES':
-//       return state;
-//     case 'ADD_NOTE':
-//       return [...state, action.note]
-//     case 'DELETE_NOTE':
-//       idx = state.findIndex(note => note.id === action.id)
-//       return [...state.slice(0, idx), ...state.slice(idx + 1)];
-//     default:
-//       return state;
-//   }
-// }
